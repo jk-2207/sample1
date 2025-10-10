f3403d847752d4ef6254868238eab6c5a9576781
jk-2207 <jkrishitha@gmail.com>
2025-10-10 20:45:40 +0530

    Update test_streak.py

--- tests/test_streak.py
+++ tests/test_streak.py
@@ -1,26 +1,20 @@
-import pytest
 from streak import longest_positive_streak
 
-def test_empty_list():
+def test_empty_input():
     assert longest_positive_streak([]) == 0
 
-def test_no_positive_numbers():
-    assert longest_positive_streak([-1, -2, -3, 0]) == 0
-
-def test_all_positive_numbers():
-    assert longest_positive_streak([1, 2, 3, 4, 5]) == 5
-
-def test_single_streak():
-    assert longest_positive_streak([1, 2, 0, 3, 4, 5]) == 3
-
 def test_multiple_streaks():
-    assert longest_positive_streak([1, 2, -1, 3, 4, 0, 5, 6, 7]) == 3
+    nums = [2, 3, -1, 5, 6, 7, 0, 4]
+    assert longest_positive_streak(nums) == 3
 
-def test_streak_at_the_beginning():
-    assert longest_positive_streak([1, 2, 3, -1, 0, -2]) == 3
+def test_with_zeros_and_negatives():
+    nums = [1, 0, 1, 1, -2, 3]
+    assert longest_positive_streak(nums) == 2
 
-def test_streak_at_the_end():
-    assert longest_positive_streak([-1, 0, 1, 2, 3, 4]) == 4
+def test_all_positive():
+    nums = [1, 1, 1]
+    assert longest_positive_streak(nums) == 3
 
-def test_zeros_and_negatives():
-    assert longest_positive_streak([1, 0, 2, -3, 4, 5, 0, 6]) == 2
+def test_all_non_positive():
+    nums = [0, -1, 0, -2]
+    assert longest_positive_streak(nums) == 0
